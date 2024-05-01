3c8721b6a0f3ebbac3b755c1ade9170711eb8489
Mudit Mehta <muditmehta27@outlook.com>
2024-05-02 03:28:48 +0530

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+model_id = "dreamlike-art/dreamlike-photoreal-2.0"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+prompt = "photo, a church in the middle of a field of crops, bright cinematic lighting, gopro, fisheye lens"
+image = pipe(prompt).images[0]
+
+image.save("./result.jpg")
