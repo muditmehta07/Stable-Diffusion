b61dfc7216df53a5dbcdeadd3e2410b95cd8b295
Mudit Mehta <muditmehta27@outlook.com>
2024-05-02 17:32:08 +0530

    new

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-from diffusers import StableDiffusionPipeline
+import os
 import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+torch.cuda.empty_cache()
 
-model_id = "dreamlike-art/dreamlike-photoreal-2.0"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+SDV5_MODEL_PATH = "./stable-diffusion-v1-5"
+SAV_PATH = "./media"
 
-prompt = "photo, a church in the middle of a field of crops, bright cinematic lighting, gopro, fisheye lens"
-image = pipe(prompt).images[0]
+prompt = input("Prompt to generate image: ")
 
-image.save("./result.jpg")
+num_inference_steps = 100
+height, width = 512, 720
+
+def uniquify(path):
+    filename, extension = os.path.splitext(path)
+    counter = 1
+
+    while os.path.exists(path):
+        path = f"{filename}({counter}){extension}"
+        counter+=1
+
+    return path
+
+print(f"Characters: {len(prompt)} -- limit: 200")
+
+pipe = StableDiffusionPipeline.from_pretrained(SDV5_MODEL_PATH)
+pipe = pipe.to('cpu')
+
+with autocast('cpu'):
+    image = pipe(
+        prompt, 
+        height=height, 
+        width=width
+    ).images[0]
+
+image_path = uniquify(os.path.join(SAV_PATH, (prompt[:25] + '...') if len(prompt)>25 else prompt) + '.png')
+print(image_path)
+image.save(image_path)
