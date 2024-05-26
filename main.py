4af5c46741a3e09685f17d39ea16e96b8da7694e
Mudit Mehta <70369287+muditmehta07@users.noreply.github.com>
2024-05-26 16:33:24 +0530

    update

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ from torch import autocast
 from diffusers import StableDiffusionPipeline
 torch.cuda.empty_cache()
 
+from prompt import art_styles
+
 SDV5_MODEL_PATH = "D:\Models\stable-diffusion-v1-5"
 SAV_PATH = "media"
 
 prompt = input("Prompt to generate image: ")
+num_images_per_prompt = 5
 
 num_inference_steps = 100
 height, width = 512, 720
+
+device_type = 'cuda'
+low_vram = True
+
+negative_prompt = "bad, low quality"
+
 
 def uniquify(path):
     filename, extension = os.path.splitext(path)
@@ -22,17 +31,58 @@ def uniquify(path):
         counter+=1
     return path
 
-print(f"Characters: {len(prompt)} -- limit: 200")
-pipe = StableDiffusionPipeline.from_pretrained(SDV5_MODEL_PATH)
-pipe = pipe.to('cpu')
+def render_prompt():
+    shortened_prompt = (prompt[:25] + '...') if len(prompt)>25 else prompt
+    shortened_prompt = shortened_prompt.replace(' ', '_')
 
-with autocast('cpu'):
-    image = pipe(
-        prompt, 
-        height=height, 
-        width=width
-    ).images[0]
+    generation_path = os.path.join(SAV_PATH, shortened_prompt.removesuffix('...'))
 
-image_path = uniquify(os.path.join(SAV_PATH, (prompt[:25] + '...') if len(prompt)>25 else prompt) + '.png')
-print(image_path)
-image.save(image_path)
+    if not os.path.exists(SAV_PATH):
+        os.mkdir(SAV_PATH)
+    if not os.path.exists(generation_path):
+        os.mkdir(generation_path)
+
+    if device_type == 'cuda':
+        if low_vram:
+            pipe = StableDiffusionPipeline.from_pretrained(
+                torch_dtype = torch.float16,
+                revision = 'fp16'
+            )
+        else:
+            pipe = StableDiffusionPipeline.from_pretrained(SDV5_MODEL_PATH)
+
+        pipe = pipe.to('cuda')
+
+        if low_vram:
+            pipe.enable_attention_slicing()
+    
+    elif device_type == 'cpu':
+        pipe = StableDiffusionPipeline.from_pretrained(SDV5_MODEL_PATH)
+    else:
+        print('Invalid Device Type')
+        return
+
+    for style_type, style_prompt in art_styles.items():
+        prompt_stylized = f"{prompt}, {style_prompt}"
+
+        print(f"Full Prompt:\n{prompt_stylized}\n")
+        print(f"Characters: {len(prompt)} -- limit: 200")
+
+        for i in range(num_images_per_prompt):
+            if device_type == 'cuda':
+                with autocast('cuda'):
+                    image = pipe(
+                        prompt_stylized,
+                        negative_prompt=negative_prompt,
+                        height=height,
+                        width=width
+                    ).images[0]
+            else:
+                image = pipe(prompt).images[0]
+
+            image_path = uniquify(os.path.join(SAV_PATH, shortened_prompt + '.png')
+            print(image_path)
+            image.save(image_path)
+    print("Render Finished")
+
+render_prompt()
---
+++ prompt.py
@@ -0,0 +1,6 @@
+art_styles = {
+    'None': '',
+    '3D render': 'unreal engine render, octane render, 4k, depth of field, ultra realistic',
+    'Digital Art': 'digital art, digital painting',
+    'Fine Art': 'oil painting, golden ratio, painting'
+}
